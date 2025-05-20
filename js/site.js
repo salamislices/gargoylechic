978c52d9c5d9b23f2b03e9a33d4abe85f587fad5
danielf <freyser5625@gmail.com>
2025-05-19 20:27:30 -0700

    done

--- js/site.js
+++ js/site.js
@@ -2,19 +2,16 @@
 // Author: Your Name
 // Date:
 
-// Constants
-
-// Functions
-$("#make-convo").click(function(){
-  // get new fake dialogue
+$(function() {
+  $("#make-convo").click(function(){
     const strings = ["I love you!", "Hey sweet thing", "Ur so sweet", "I miss u!", "mwuah"];
     const newText = strings[Math.floor(Math.random() * strings.length)];
 
-  // append a new div to our output div
     $("#output").find(".text").remove();
     $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+  });
 });
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
+
 function myFunction(param1, param2) {
   // some code here
   // return results;
@@ -25,5 +22,4 @@ function main() {
   // the code that makes everything happen
 }
 
-// let's get this party started
 main();
