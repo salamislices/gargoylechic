9cb321f145e3a27af5daa302b9bbbc33447e9096
danielf <freyser5625@gmail.com>
2025-05-26 17:47:23 -0700

    push

--- js/site.js
+++ js/site.js
@@ -12,6 +12,44 @@ $(function() {
   });
 });
 
+const canvas = document.getElementById('paintCanvas');
+if (canvas) {
+  const ctx = canvas.getContext('2d');
+  let painting = false;
+
+  function startPosition(e) {
+    painting = true;
+    draw(e);
+  }
+
+  function endPosition() {
+    painting = false;
+    ctx.beginPath();
+  }
+
+  function draw(e) {
+    if (!painting) return;
+    ctx.lineWidth = 5;
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = '#ff3399';
+    const rect = canvas.getBoundingClientRect();
+    ctx.lineTo(e.clientX - rect.left, e.clientY - rect.top);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(e.clientX - rect.left, e.clientY - rect.top);
+  }
+
+  function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  canvas.addEventListener('mousedown', startPosition);
+  canvas.addEventListener('mouseup', endPosition);
+  canvas.addEventListener('mousemove', draw);
+
+  window.clearCanvas = clearCanvas;
+}
+
 function myFunction(param1, param2) {
   // some code here
   // return results;
